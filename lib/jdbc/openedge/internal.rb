c93827fa458dd4ed098733365db0c0f89457cbd3
Abe Voelker <abe@abevoelker.com>
2012-07-22 10:26:10 -0500

    Add code for 10.2A

---
+++ lib/jdbc/openedge/internal.rb
@@ -0,0 +1,22 @@
+module Jdbc
+  module OpenEdge
+    module Internal
+      JARS = [ 'base.jar', 'openedge.jar', 'util.jar']
+    end
+  end
+end
+
+if RUBY_PLATFORM =~ /java/
+  Jdbc::OpenEdge::Internal::JARS.each do |j|
+    begin
+      require j
+    rescue LoadError
+      raise LoadError, <<-HERE.gsub!(/\s\s/, '')
+        Unable to load #{j}. Make sure that you've
+        put the driver .jars in your Java $CLASSPATH!
+      HERE
+    end
+  end
+elsif $VERBOSE
+  warn "jdbc-openedge-internal is only for use with JRuby!"
+end
---
+++ lib/jdbc/openedge/internal/version.rb
@@ -0,0 +1,7 @@
+module Jdbc
+  module OpenEdge
+    module Internal
+      VERSION = '10.2.1.0'
+    end
+  end
+end
